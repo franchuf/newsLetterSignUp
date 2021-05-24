4b4dade2eb517e7b3d56e678a33e264b50c5f7fa
Francisco Faner <franchuf@gmail.com>
2021-05-24 16:18:15 -0300

    configuration

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
-const request = require ("request");
+const request = require("request");
+const app = express ();
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public")); 
 
- 
+app.get("/", function(req,res){
+    console.log(__dirname)
+    res.sendFile(__dirname + "/signup.html"); 
+})
 
- const app = express ();
- app.listen (3000, function(){
-     console.log("server running on port 3000")
+app.post("/",function(req,res){
+    var firstName = req.body.fName
+    var lastName = req.body.lName
+    var email = req.body.email
+    console.log (firstName, lastName, email)
+})
+
+app.listen (3009, function(){
+     console.log("server running on port 3009")
  })
- 
  
