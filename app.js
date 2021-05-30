391b865587e0bdfecf343f0b50a5ce36bbd1d01d
Francisco Faner <franchuf@gmail.com>
2021-05-30 19:47:39 -0300

    no puedo levantar el error 'https is not defined'

--- app.js
+++ app.js
@@ -11,10 +11,38 @@ app.get("/", function(req,res){
 })
 
 app.post("/",function(req,res){
-    var firstName = req.body.fName
-    var lastName = req.body.lName
-    var email = req.body.email
-    console.log (firstName, lastName, email)
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+    const data = {
+        members: [
+            {
+                email_address: email, 
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data)
+    const url = "https://us6.api.mailchimp.com/3.0/lists/5f2efa8e62"
+    const options = {
+        method: "POST",
+        auth: "francho:8d95a13fea1643515f381d24dabb76b4-us6"
+    }
+    
+    const request =  https.request(url, options,function (response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+
+    })
+
+    request.write(jsonData);
+    request.end();
+
 })
 
 app.listen (5503, function(){
@@ -24,3 +52,8 @@ app.listen (5503, function(){
 
 
 
+//API KEY
+// 8d95a13fea1643515f381d24dabb76b4-us6
+
+//LIST ID
+//5f2efa8e62
